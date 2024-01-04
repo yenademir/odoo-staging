f354c1d222a286ef2c46dba824ca046b9b4311b5
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-04 23:51:18 +0300

    Update timesheet_development.py

--- yena_timesheet_development/models/timesheet_development.py
+++ yena_timesheet_development/models/timesheet_development.py
@@ -4,6 +4,5 @@ class TimeSheet(models.Model):
     _inherit = 'account.analytic.line'
     planned_hours = fields.Float(
         string='Hours Planned',
-        related="task_id.planned_hours",
         readonly=False
     )
