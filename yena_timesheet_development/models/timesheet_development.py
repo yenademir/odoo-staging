2dd4ab04987edf7e83b760f5c5d4e7d821176fea
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-04 23:43:26 +0300

    Update timesheet_development.py

--- yena_timesheet_development/models/timesheet_development.py
+++ yena_timesheet_development/models/timesheet_development.py
@@ -7,10 +7,3 @@ class TimeSheet(models.Model):
         related="task_id.planned_hours",
         readonly=False
     )
-
-
-    @api.onchange('planned_hours')
-    def _onchange_planned_hours(self):
-        for line in self:
-            if line.task_id:
-                line.task_id.write({'planned_hours': line.planned_hours})
