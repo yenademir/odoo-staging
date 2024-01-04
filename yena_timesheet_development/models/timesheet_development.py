a9501c8b758c9486c290e495a53813b286e15a3f
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-05 00:07:22 +0300

    Delete yena_timesheet_development directory

--- yena_timesheet_development/models/timesheet_development.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-class TimeSheet(models.Model):
-    _inherit = 'account.analytic.line'
-    planned_hours = fields.Float(
-        string='Hours Planned',
-        readonly=False,
-        store=True
-    )
