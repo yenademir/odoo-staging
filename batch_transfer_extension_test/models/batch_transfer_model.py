72ce6b56f96a3f594264d4a97e270115bc191c71
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-09 01:43:10 +0300

    Update batch_transfer_model.py

--- batch_transfer_extension_test/models/batch_transfer_model.py
+++ batch_transfer_extension_test/models/batch_transfer_model.py
@@ -202,10 +202,10 @@ class StockPickingBatch(models.Model):
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    project_ids = fields.Many2one('project.project', string='Projects', compute='_compute_project_transfer', store=True)
+    project_ids = fields.Many2one('project.project', string='Projects', compute='_compute_project_transfer_line', store=True)
 
     @api.depends('picking_id.project_transfer')
-    def _compute_project_transfer(self):
+    def _compute_project_transfer_line(self):
         for record in self:
             record.project_ids = record.picking_id.project_transfer
             
