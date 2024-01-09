aec549ad5234a60ee349b2687b713c7df04cadfd
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-09 16:56:47 +0300

    Update inventory_development.py

--- yena_inventory_development_test/models/inventory_development.py
+++ yena_inventory_development_test/models/inventory_development.py
@@ -7,22 +7,6 @@ class Picking(models.Model):
     _inherit = 'stock.picking'
 
     arrive_date = fields.Date(string="Arrive Date")
-    situation = fields.Selection(
-        [("to_be_planned", "To Be Planned"),
-         ("on_the_way", "On The Way"),
-         ("arrived", "Arrived")],
-        string="Situation",
-        related="batch_id.situation",
-        store=True,
-        readonly=False
-    )
-    
-    transportation_code = fields.Char(
-        string="Transportation Code",
-        related="batch_id.transportation_code",
-        store=True,
-        readonly=False
-    )
     sale_id=fields.Many2one("sale.order",string="Sale Order")
     purchase_id=fields.Many2one("purchase.order",string="Purchase Order")
     sequence_code = fields.Char(string='Sequence Code', related='picking_type_id.sequence_code', store=True)
