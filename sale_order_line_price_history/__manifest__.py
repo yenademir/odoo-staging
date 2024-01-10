02ccdd9c88bedbf7244ffbea235d3ef17375c007
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-10 15:22:16 +0300

    Add files via upload

---
+++ sale_order_line_price_history/__manifest__.py
@@ -0,0 +1,17 @@
+{
+     "name": "Yena Sale order line price history",
+    "version": "15.0.1.0.0",
+    "category": "Purchase Management",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "license": "AGPL-3",
+    
+    "depends": ["sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/sale_order_line_history.xml",
+        "views/sale_views.xml",
+    ],
+    
+    "installable": True,
+}
