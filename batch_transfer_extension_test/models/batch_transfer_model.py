da1c9507244af3ec9234896e99f3c06f0e5be017
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-14 03:24:49 +0300

    Update batch_transfer_model.py

--- batch_transfer_extension_test/models/batch_transfer_model.py
+++ batch_transfer_extension_test/models/batch_transfer_model.py
@@ -81,7 +81,7 @@ class StockPickingBatch(models.Model):
     edespatch_sender_id = fields.Many2one(
         'edespatch.sender', 
         string='e-Despatch Sender', 
-        default=lambda self: 1,
+        default=lambda self: self.env.ref('l10n_tr_stock_edespatch.edespatch_sender_1_69303314', raise_if_not_found=False),
         domain=[('code', 'in', ['urn:mail:irsaliyegb@yenaengineering.nl'])],
         inverse='_inverse_edespatch_sender_id'
     )
@@ -89,7 +89,7 @@ class StockPickingBatch(models.Model):
     edespatch_postbox_id = fields.Many2one(
         'edespatch.postbox', 
         string='e-Despatch Postbox',
-        default=lambda self: 639,
+        default=lambda self: self.env.ref('edespatch.edespatch_postbox_639_00357d39', raise_if_not_found=False),
         domain=[('code', 'in', ['urn:mail:irsaliyepk@gib.gov.tr'])],
         inverse='_inverse_edespatch_postbox_id'
     )
