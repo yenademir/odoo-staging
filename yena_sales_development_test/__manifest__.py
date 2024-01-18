73b527f8ab41296fb08ec9220db7c75a0a92bb31
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-18 08:27:36 +0300

    Add files via upload

---
+++ yena_sales_development_test/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "YENA Sale Development",
+    'version': '15.1.0',
+    'summary': "All Development about sale.order",
+    'author': "Emre MATARACI,Alperen Alihan ER, Selçuk Atav",
+    'website': "https://yenaengineering.nl",
+    'category': 'Inventory',
+    'license': 'LGPL-3',
+    'depends': ['sale', 'crm'],
+    'data': [
+        'views/sales_development.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
