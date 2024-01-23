89a008bbdfcd6b7ad6c8af4b7b349e095883fa61
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-23 15:00:41 +0300

    Update __manifest__.py

--- yena_inventory_development_test/__manifest__.py
+++ yena_inventory_development_test/__manifest__.py
@@ -2,15 +2,16 @@
     'name': "YENA Inventory Development",
     'version': '15.1.1',
     'summary': "All Development about product.template and stock.picking",
-    'author': "Alperen Alihan ER",
+    'author': "Emre MATARACI, Alperen Alihan ER, Selçuk ATAV",
     'website': "https://yenaengineering.nl",
     'category': 'Inventory',
     'license': 'LGPL-3',
-    'depends': ['product', 'stock', 'barcodes', 'digest', 'purchase', 'base', 'account', 'delivery', 'batch_transfer_extension_test'],
+    'depends': ['product', 'stock', 'barcodes', 'digest', 'purchase', 'base', 'account', 'delivery' , 'batch_transfer_extension'],
     'data': [
         'views/inventory_development.xml',
         'security/ir.model.access.csv',
-        'views/hs_code.xml'
+        'views/hs_code.xml',
+        'data/mail_activity_data.xml'
     ],
     'installable': True,
     'auto_install': False,
