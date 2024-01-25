3ce6bf5e5868e96a6f52855b0e640e475db6c295
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-25 12:06:53 +0300

    Update accounting_development.py

--- yena_accounting_development/models/accounting_development.py
+++ yena_accounting_development/models/accounting_development.py
@@ -3,17 +3,18 @@ from odoo import api, fields, models
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    customer_reference = fields.Char(string='Customer Reference', compute='_compute_customer_reference', store=True, readonly=False)
+    customer_reference = fields.Char(string='Müşteri Referansı', compute='_compute_customer_reference', store=True, readonly=False)
 
-    @api.depends('move_id.invoice_origin')
+    @api.depends('sale_line_ids.order_id')
     def _compute_customer_reference(self):
         for record in self:
-            sale_order = self.env['sale.order'].search([('name', '=', record.move_id.invoice_origin)], limit=1)
-            if sale_order:
-                record.customer_reference = sale_order.customer_reference
+            if record.sale_line_ids:
+                # Fatura satırı ile ilişkili ilk satış siparişi satırını al
+                sale_order_line = record.sale_line_ids[0]
+                record.customer_reference = sale_order_line.order_id.customer_reference
             else:
                 record.customer_reference = False
-                
+
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
@@ -22,5 +23,6 @@ class AccountMove(models.Model):
     @api.depends('invoice_line_ids.customer_reference')
     def _compute_customer_references(self):
         for record in self:
+            # Farklı customer_reference değerlerini al
             references = list(set(line.customer_reference for line in record.invoice_line_ids if line.customer_reference))
             record.customer_references = ', '.join(references)
