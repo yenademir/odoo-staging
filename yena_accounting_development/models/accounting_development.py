68a6da79e8bb834f2e6668a4e29a03311b28456d
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-26 16:27:48 +0300

    Update accounting_development.py

--- yena_accounting_development/models/accounting_development.py
+++ yena_accounting_development/models/accounting_development.py
@@ -3,20 +3,21 @@ from odoo import api, fields, models
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    customer_reference = fields.Char(string='Müşteri Referansı', compute='_compute_customer_reference', readonly=False)
+    customer_reference = fields.Char(string='Müşteri Referansı', compute='_compute_customer_reference', store=True, readonly=False)
 
     @api.depends('move_id.invoice_origin')
     def _compute_customer_reference(self):
         for record in self:
+            references = []
+            # invoice_origin alanındaki her bir sipariş ismi için arama yap
             if record.move_id.invoice_origin:
-                # Faturanın kökenini kullanarak ilgili satış siparişini bul
-                sale_order = self.env['sale.order'].search([('name', '=', record.move_id.invoice_origin)], limit=1)
-                if sale_order:
-                    record.customer_reference = sale_order.customer_reference
-                else:
-                    record.customer_reference = False
-            else:
-                record.customer_reference = False
+                order_names = record.move_id.invoice_origin.split(', ')
+                for order_name in order_names:
+                    sale_order = self.env['sale.order'].search([('name', '=', order_name.strip())], limit=1)
+                    if sale_order:
+                        references.append(sale_order.customer_reference)
+            # Benzersiz referansları set olarak sakla ve birleştir
+            record.customer_reference = ', '.join(set(references))
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -27,8 +28,5 @@ class AccountMove(models.Model):
     def _compute_customer_references(self):
         for record in self:
             # Fatura satırlarındaki benzersiz müşteri referanslarını topla
-            references = set()
-            for line in record.invoice_line_ids:
-                if line.customer_reference:
-                    references.add(line.customer_reference)
+            references = list(set(line.customer_reference for line in record.invoice_line_ids if line.customer_reference))
             record.customer_references = ', '.join(references)
