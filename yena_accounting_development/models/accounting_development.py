32e90dc5f586b42c1a453932598ee22bbc1a3e40
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-28 01:21:39 +0300

    Update accounting_development.py

--- yena_accounting_development/models/accounting_development.py
+++ yena_accounting_development/models/accounting_development.py
@@ -3,28 +3,27 @@ from odoo import models, fields, api
 class AccountMoveLineInherit(models.Model):
     _inherit = 'account.move.line'
 
-    customer_reference = fields.Char(string='Customer Reference')
+    customer_reference = fields.Char(string='Müşteri Referansı')
 
     @api.model
     def create(self, vals):
-        if 'sale_line_ids' in vals and vals['sale_line_ids']:
-            sale_line = self.env['sale.order.line'].browse(vals['sale_line_ids'][0])
-            vals['customer_reference'] = sale_line.order_id.customer_reference
+        sale_line_id = vals.get('sale_line_ids', [])
+        if sale_line_id:
+            sale_line = self.env['sale.order.line'].browse(sale_line_id[0][2][0])  # sale_line_ids alanından ilk sale.order.line ID'sini alır
+            if sale_line and sale_line.order_id:
+                vals['customer_reference'] = sale_line.order_id.customer_reference
         return super(AccountMoveLineInherit, self).create(vals)
 
 class AccountMoveInherit(models.Model):
     _inherit = 'account.move'
 
-    customer_references = fields.Char(string='Customer References', compute='_compute_customer_references', store=True)
+    customer_references = fields.Char(string='Müşteri Referansları', compute='_compute_customer_references')
 
     @api.depends('invoice_line_ids.customer_reference')
     def _compute_customer_references(self):
         for record in self:
             references = record.invoice_line_ids.mapped('customer_reference')
-            # Flatten the list of lists into a single list of references
-            flattened_references = [item for sublist in references for item in (sublist if isinstance(sublist, list) else [sublist])]
-            # Filter out any non-string items and ensure uniqueness
-            unique_references = set(filter(lambda x: isinstance(x, str), flattened_references))
-            # Join the unique string references with a comma
-            record.customer_references = ','.join(unique_references)
-
+            # Boş olmayan ve string türündeki değerleri filtrele
+            valid_references = {ref for ref in references if ref and isinstance(ref, str)}
+            # Benzersiz string değerleri virgülle birleştir
+            record.customer_references = ','.join(valid_references)
