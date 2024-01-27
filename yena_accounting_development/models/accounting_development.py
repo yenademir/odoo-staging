4623cc9d6c9c356931ed4a16966efb1f08889dc7
yenademir <116823062+yenademir@users.noreply.github.com>
2024-01-28 01:13:03 +0300

    Update accounting_development.py

--- yena_accounting_development/models/accounting_development.py
+++ yena_accounting_development/models/accounting_development.py
@@ -1,28 +1,24 @@
-from odoo import api, fields, models
+from odoo import models, fields, api
 
-class AccountMoveLine(models.Model):
+class AccountMoveLineInherit(models.Model):
     _inherit = 'account.move.line'
 
-    customer_reference = fields.Char(string='Müşteri Referansı')
+    customer_reference = fields.Char(string='Customer Reference')
 
     @api.model
     def create(self, vals):
-        # Yeni bir account.move.line kaydı oluşturulurken, ilişkili sale.order'dan customer_reference al
-        record = super(AccountMoveLine, self).create(vals)
-        if record.move_id.invoice_origin:
-            order_name = record.move_id.invoice_origin.split(', ')[0].strip()
-            sale_order = self.env['sale.order'].search([('name', '=', order_name)], limit=1)
-            if sale_order:
-                record.customer_reference = sale_order.customer_reference
-        return record
+        if 'sale_line_ids' in vals and vals['sale_line_ids']:
+            sale_line = self.env['sale.order.line'].browse(vals['sale_line_ids'][0])
+            vals['customer_reference'] = sale_line.order_id.customer_reference
+        return super(AccountMoveLineInherit, self).create(vals)
 
-class AccountMove(models.Model):
+class AccountMoveInherit(models.Model):
     _inherit = 'account.move'
 
-    customer_references = fields.Char(compute='_compute_customer_references', string='Müşteri Referansları')
+    customer_references = fields.Char(string='Customer References', compute='_compute_customer_references', store=True)
 
     @api.depends('invoice_line_ids.customer_reference')
     def _compute_customer_references(self):
         for record in self:
-            references = list(set(line.customer_reference for line in record.invoice_line_ids if line.customer_reference))
-            record.customer_references = ', '.join(references)
+            references = record.invoice_line_ids.mapped('customer_reference')
+            record.customer_references = ','.join(set(references))
