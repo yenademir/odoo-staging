a65fb673a09982ef71af2675d5044c69924c99e9
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-07 01:50:33 +0300

    Update inventory_development.py

--- yena_inventory_development_test/models/inventory_development.py
+++ yena_inventory_development_test/models/inventory_development.py
@@ -21,7 +21,6 @@ class Picking(models.Model):
         readonly=False
     )
     sale_id=fields.Many2one("sale.order",string="Sale Order")
-    purchase_id=fields.Many2one("purchase.order",string="Purchase Order")
     sequence_code = fields.Char(string='Sequence Code', related='picking_type_id.sequence_code', store=True)
     logistic_company = fields.Char (string="Logistic Company", store=True)
 
