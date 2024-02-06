c6f5e1482cc712e4cc1ad2bff5211b8581d5d503
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-07 01:44:04 +0300

    Update inventory_development.py

--- yena_inventory_development_test/models/inventory_development.py
+++ yena_inventory_development_test/models/inventory_development.py
@@ -103,7 +103,7 @@ class StockMove(models.Model):
     #arrival_date = fields.Date(related='picking_id.arrival_date', store=True, readonly=True)
     purchase_id=fields.Many2one(related='picking_id.purchase_id',string="Purchase Order")
     edespatch_date=fields.Datetime(related='picking_id.edespatch_date',string="Actual Departure Date")
-    airtag_url = fields.Char(string='Airtag Link', related='picking_id.batch_id.airtag_url', store=True, readonly=True)
+    #airtag_url = fields.Char(string='Airtag Link', related='picking_id.batch_id.airtag_url', store=True, readonly=True)
     vehicle_type_id = fields.Many2one(string='Vehicle Type', related='picking_id.batch_id.vehicle_type_id', store=True, readonly=True)
     
 class ProductTemplate(models.Model):
