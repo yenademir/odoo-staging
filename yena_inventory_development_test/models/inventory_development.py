bb578cfe6439b6a19d01b78931579f6a11b0e6b6
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-06 16:50:43 +0300

    Update inventory_development.py

--- yena_inventory_development_test/models/inventory_development.py
+++ yena_inventory_development_test/models/inventory_development.py
@@ -38,20 +38,6 @@ class Picking(models.Model):
             'user_id': self.env.user.id,
         }
 
-    def button_validate(self):
-        for record in self:
-            res = super(Picking, record).button_validate()
-            if record.state == 'done' and record.picking_type_id.id == 2:
-                existing_activities = self.env['mail.activity'].search([
-                    ('res_model_id.model', '=', 'stock.picking'),
-                    ('res_id', '=', record.id),
-                    ('activity_type_id', '=', self.env.ref('yena_inventory_development.activity_type_custom').id)
-                ])
-                if not existing_activities:
-                    activity_vals = record._create_scheduled_activity()
-                    self.env['mail.activity'].create(activity_vals)
-        return res
-
     
     def _update_scheduled_date(self, vals):
         if 'scheduled_date' not in vals:
