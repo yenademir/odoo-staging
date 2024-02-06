ae79e21eae319997347719980927c723a2fba205
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-07 02:01:04 +0300

    Update inventory_development.py

--- yena_inventory_development_test/models/inventory_development.py
+++ yena_inventory_development_test/models/inventory_development.py
@@ -89,11 +89,12 @@ class Picking(models.Model):
 
         return defaults
 
-
+    
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     project_transfer = fields.Many2many(related="picking_id.project_transfer", string="Project Number")
+    picking_type_id = fields.Many2one(related="picking_id.picking_type_id", string="Operation Type", store=True)
     related_partner = fields.Many2one(related="picking_id.partner_id", string="Receive From / Delivery Adress", store=True)
     situation = fields.Selection(related="picking_id.situation", string="Situation", store=True)
     transportation_code = fields.Char(related="picking_id.transportation_code", string="Transportation Code", store=True)
@@ -101,6 +102,7 @@ class StockMove(models.Model):
     edespatch_delivery_type = fields.Selection(related="picking_id.edespatch_delivery_type", string="Delivery Type")
     scheduled_date = fields.Datetime(related='picking_id.scheduled_date', store=True, readonly=True)
     arrival_date = fields.Date(related='picking_id.arrival_date', store=True, readonly=True)
+    purchase_id=fields.Many2one(related='picking_id.purchase_id',string="Purchase Order")
     edespatch_date=fields.Datetime(related='picking_id.edespatch_date',string="Actual Departure Date")
     airtag_url = fields.Char(string='Airtag Link', related='picking_id.batch_id.airtag_url', store=True, readonly=True)
     vehicle_type_id = fields.Many2one(string='Vehicle Type', related='picking_id.batch_id.vehicle_type_id', store=True, readonly=True)
