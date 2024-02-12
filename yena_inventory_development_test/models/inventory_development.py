73b96cc42ff0baf69ea18d95bfacd8028b5e192c
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-12 13:20:59 +0300

    Update inventory_development.py

--- yena_inventory_development_test/models/inventory_development.py
+++ yena_inventory_development_test/models/inventory_development.py
@@ -23,7 +23,7 @@ class Picking(models.Model):
     sale_id=fields.Many2one("sale.order",string="Sale Order")
     purchase_id=fields.Many2one("purchase.order",string="Purchase Order")
     sequence_code = fields.Char(string='Sequence Code', related='picking_type_id.sequence_code', store=True)
-    logistic_company = fields.Char (string="Logistic Company", store=True)
+    logistic_company = fields.Many2one('res.partner', string='Logistic Company', domain=[('is_company', '=', True)])
 
     @api.model
     def create(self, vals):
