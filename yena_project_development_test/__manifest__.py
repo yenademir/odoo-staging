7ce77280ec2d7b778721468d5c36162fe07a6daf
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-12 16:58:22 +0300

    Add files via upload

---
+++ yena_project_development_test/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "YENA Project Development",
+    'version': '15.1.0',
+    'summary': "All Development about project.project",
+    'author': "Selçuk Atav",
+    'website': "https://yenaengineering.nl",
+    'category': 'Project',
+    'license': 'LGPL-3',
+    'depends': ['project'],
+    'data': [
+        'views/project_development.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ yena_project_development_test/models/project_development.py
@@ -0,0 +1,101 @@
+from odoo import models,fields,api
+
+class Project(models.Model):
+    _inherit = 'project.project'
+
+    sales_count = fields.Integer(string="Sales", compute="_compute_sales_count")
+    purchase_count = fields.Integer(string='Purchases', compute='_compute_purchase_count')
+    delivery_count = fields.Integer(string="Delivery Count",compute="_compute_delivery_count")
+    #delivery inventory 
+    
+
+    def action_show_sales(self):
+        self.ensure_one()
+        sale_ids = []
+
+        # Bu projeye ait satın alma siparişlerini bul
+        sales = self.env['sale.order'].search([('project_sales', '=', self.id)])
+
+        for sale in sales:
+            sale_ids.append(sale.id)
+
+        context = {
+            'default_project_sales': self.id,  # Satın alma emri oluşturulurken projenin ID'sini varsayılan olarak ayarla
+            'search_default_project_sales': self.id,  # Listeyi sadece bu projeye ait satın alma siparişleriyle sınırla
+            'create': True,
+        }
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Sales',
+            'view_mode': 'tree,form',
+            'res_model': 'sale.order',
+            'domain': [('id', 'in', sale_ids)],
+            'context': context,
+        }
+    
+    def action_show_purchases(self):
+        self.ensure_one()
+        purchase_ids = []
+
+        # Bu projeye ait satın alma siparişlerini bul
+        purchases = self.env['purchase.order'].search([('project_purchase', '=', self.id)])
+
+        for purchase in purchases:
+            purchase_ids.append(purchase.id)
+
+        context = {
+            'default_project_purchase': self.id,  # Satın alma emri oluşturulurken projenin ID'sini varsayılan olarak ayarla
+            'search_default_project_purchase': self.id,  # Listeyi sadece bu projeye ait satın alma siparişleriyle sınırla
+            'create': True,
+        }
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Purchases',
+            'view_mode': 'tree,form',
+            'res_model': 'purchase.order',
+            'domain': [('id', 'in', purchase_ids)],
+            'context': context,
+        }
+
+    def action_show_transfers(self):
+        self.ensure_one()
+        transfer_ids = []
+
+        # Batch transferin adı ile eşleşen purchase'ları bul
+        sales = self.env['stock.picking'].search([('project_transfer', '=', self.name)])
+
+        for sale in sales:
+            transfer_ids.append(sale.id)
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Sales',
+            'view_mode': 'tree,form',
+            'res_model': 'stock.picking',
+            'domain': [('id', 'in', transfer_ids)],
+            'context': {'create': False},
+        }
+    
+
+    def _compute_purchase_count(self):
+         for project in self:
+             purchases = self.env['purchase.order'].search([
+                 ('project_purchase', '=', project.name)
+             ])
+             project.purchase_count = len(purchases)
+
+    def _compute_sales_count(self):
+         for project in self:
+             sales = self.env['sale.order'].search([
+                 ('project_sales', '=', project.name)
+             ])
+             project.sales_count = len(sales)
+
+    def _compute_delivery_count(self):
+         for project in self:
+             deliveries = self.env['stock.picking'].search([
+                 ('project_transfer', '=', project.name)
+             ])
+             project.delivery_count = len(deliveries)
