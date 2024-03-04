4eea02ba769253f7270dcfc73dbbb58bbc745a08
yenademir <116823062+yenademir@users.noreply.github.com>
2024-03-04 17:25:49 +0300

    YP-166 Purchase Blanket Order Status

--- yena_purchase_blanket_order_development_test/models/purchase_blanket_order_development.py
+++ yena_purchase_blanket_order_development_test/models/purchase_blanket_order_development.py
@@ -9,13 +9,15 @@ class PurchaseBlanketOrder(models.Model):
     remaining_total = fields.Monetary(compute='_compute_totals', store=True, string='Remaining Total')
 
 
-    @api.depends('line_ids.invoiced_subtotal', 'line_ids.ordered_subtotal', 'line_ids.remaining_invoice_subtotal', 'line_ids.remaining_subtotal')
+    @api.depends('line_ids.invoiced_subtotal', 'line_ids.ordered_subtotal', 'line_ids.remaining_invoice_subtotal', 'line_ids.remaining_subtotal','amount_untaxed')
     def _compute_totals(self):
         for order in self:
             order.invoiced_total = sum(line.invoiced_subtotal for line in order.line_ids)
             order.ordered_total = sum(line.ordered_subtotal for line in order.line_ids)
             order.remaining_invoice_total = sum(line.remaining_invoice_subtotal for line in order.line_ids)
             order.remaining_total = sum(line.remaining_subtotal for line in order.line_ids)
+            if order.ordered_total > 0 and order.amount_untaxed > 0 and order.ordered_total == order.amount_untaxed:
+                order.state = 'done'
 
 class PurchaseBlanketOrderLine(models.Model):
     _inherit = 'purchase.blanket.order.line'
