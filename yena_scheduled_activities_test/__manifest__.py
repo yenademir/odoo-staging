5a585cfd578deee22f44daa1a55399bb3d77feae
yenademir <116823062+yenademir@users.noreply.github.com>
2024-03-05 13:34:24 +0300

    Add files via upload

---
+++ yena_scheduled_activities_test/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "YENA Scheduled Activity Development Test",
+    'version': '15.1.0',
+    'summary': "All Development about scheduled activities",
+    'author': "Emre MATARACI,Alperen Alihan ER, Selçuk Atav",
+    'website': "https://yenaengineering.nl",
+    'category': 'Inventory',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [
+        'views/scheduled_activities.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
