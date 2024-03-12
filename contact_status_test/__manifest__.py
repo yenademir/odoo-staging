30ca52ab4472430977f611819eb1ba82461d0b5b
selcukatav <64098559+selcukatav@users.noreply.github.com>
2024-03-12 10:47:19 +0300

    Add files via upload

---
+++ contact_status_test/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Contact Status (YENA)',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'Custom developments for contacts',
+    'depends': ['base', 'account'],
+    'data': [
+        'views/contact_status.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+
+}
