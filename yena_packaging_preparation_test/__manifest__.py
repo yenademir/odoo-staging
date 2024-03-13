548a9346b20733d372b2f864a2e67a6ee956dd6c
yenademir <116823062+yenademir@users.noreply.github.com>
2024-03-13 11:38:03 +0300

    Add files via upload

---
+++ yena_packaging_preparation_test/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "YENA Packaging Preparation",
+    'version': '15.1.0',
+    'summary': "Easy preparation of transfer documents",
+    'author': "Alperen Alihan ER",
+    'website': "https://yenaengineering.nl",
+    'category': 'Inventory',
+    'license': 'LGPL-3',
+    'depends': ['product', 'stock', 'barcodes', 'digest', 'purchase', 'base', 'account', 'delivery'],
+    'data': [
+        'views/packaging_preparation.xml',
+        'security/ir.model.access.csv',
+        'reports/packaging_preparation_report.xml',
+        'reports/packaging_bill_of_laid_report.xml',
+        'reports/packaging_preparation_report_delivery.xml',
+        'reports/packaging_preparation_packing_list_report.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
