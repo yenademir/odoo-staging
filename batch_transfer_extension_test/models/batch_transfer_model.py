67923299ed211cea8aa283dae8b7db5362c6e930
yenademir <116823062+yenademir@users.noreply.github.com>
2024-04-23 20:50:37 +0300

    Update batch_transfer_model.py

--- batch_transfer_extension_test/models/batch_transfer_model.py
+++ batch_transfer_extension_test/models/batch_transfer_model.py
@@ -361,8 +361,7 @@ class Picking(models.Model):
     )
 
     def _create_backorder(self, backorder_moves=[]):
-        """ Backorder oluştururken 'transportation_code' alanını kopyalamayacak şekilde düzenlenmiştir. """
-        backorder_picking = super(StockPicking, self)._create_backorder(backorder_moves)
+        backorder_picking = super()._create_backorder(backorder_moves)
         if backorder_picking:
             backorder_picking.transportation_code = '-'
         return backorder_picking
