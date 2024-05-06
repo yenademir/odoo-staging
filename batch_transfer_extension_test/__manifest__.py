ab5defdc2e61c1195b20966a8c68f272f98a1ff2
yenademir <116823062+yenademir@users.noreply.github.com>
2024-05-06 15:31:02 +0300

    Update __manifest__.py

--- batch_transfer_extension_test/__manifest__.py
+++ batch_transfer_extension_test/__manifest__.py
@@ -3,12 +3,13 @@
     'version': '1.0',
     'category': 'Inventory',
     'summary': 'Extends Batch Transfer with additional fields',
-    'author': 'Emre Mataracı',
-    'website': 'http://yourwebsite.com',
+    'author': 'Emre Mataracı, Selçuk ATAV',
+    'website': 'https://yenaengineering.nl',
     'depends': ['stock', 'project', 'purchase', 'contacts', 'l10n_tr_stock_edespatch'],
     'data': [
         'views/batch_transfer_view.xml',
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'data/email_template.xml'
     ],
     'installable': True,
     'auto_install': False,
